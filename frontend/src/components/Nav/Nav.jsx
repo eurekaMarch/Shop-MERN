5bc30b71e2bd96624b01eb1880b2ce6c6960229d
Trairat Kaewsaad <121665180+eurekaMarch@users.noreply.github.com>
2023-10-05 23:08:49 +0700

    Update Nav.jsx

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -3,16 +3,20 @@ import Logo from "/shopping.png";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Header from "./Header";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { grey } from "@mui/material/colors";
 
 function Nav() {
   return (
     <div>
       <nav>
         <Header />
-        <Link to="/">
-          <img width={60} src={Logo} alt="" />
-        </Link>
-        <span>Shopeefy</span>
+        <div>
+          <Link to="/">
+            <img width={60} src={Logo} alt="" />
+          </Link>
+          <span>Shopeefy</span>
+        </div>
 
         <TextField
           id="filled-basic"
@@ -20,10 +24,16 @@ function Nav() {
           InputLabelProps={{ style: { fontSize: 20 } }}
           inputProps={{ style: { fontSize: 16 } }}
         />
-
-        <Button variant="contained" size="large" style={{ fontSize: "1.6rem" }}>
-          Search
-        </Button>
+        <ThemeProvider theme={theme}>
+          <Button
+            variant="contained"
+            size="large"
+            style={{ fontSize: "1.6rem" }}
+            color="primary"
+          >
+            Search
+          </Button>
+        </ThemeProvider>
 
         <Link to="/register">REGISTER</Link>
         <Link to="/login">LOGIN</Link>
@@ -36,3 +46,11 @@ function Nav() {
 }
 
 export default Nav;
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: grey[900],
+    },
+  },
+});
