48a2b928b01ff4d87ea283c13fab74a08536100f
Trairat Kaewsaad <121665180+eurekaMarch@users.noreply.github.com>
2023-10-10 16:38:00 +0700

    update

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,17 +1,41 @@
-import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import Logo from "/shopping.png";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Header from "./Header";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 function Nav() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const navigate = useNavigate();
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogin = () => {
+    navigate(`/login`, { replace: true });
+  };
+
+  const handleRegister = () => {
+    navigate(`/register`, { replace: true });
+  };
+
   return (
     <div>
       <nav>
         <Header />
-        <Box
+        {/* <Box
           sx={{
             display: "flex",
             justifyContent: "space-around",
@@ -30,6 +54,7 @@ function Nav() {
             <Link to="/">
               <img width={70} src={Logo} alt="logo" />
               <Typography
+                className="Nav_text-logo"
                 sx={{
                   fontSize: "3rem",
                   fontWeight: "bold",
@@ -75,6 +100,99 @@ function Nav() {
             <Link to="/register">REGISTER</Link>
             <Link to="/login">LOGIN</Link>
           </Box>
+        </Box> */}
+
+        {/* mobile */}
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            mt: "1rem",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              position: "relative",
+              width: "21rem",
+            }}
+          >
+            <Link to="/">
+              <img width={60} src={Logo} alt="logo" />
+              <Typography
+                className="Nav_text-logo"
+                sx={{
+                  fontSize: "1.6rem",
+                  fontWeight: "bold",
+                  position: "absolute",
+                  left: "5.5rem",
+                  top: "2rem",
+                }}
+              >
+                Shopeefy
+              </Typography>
+            </Link>
+          </Box>
+          <Button
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+            endIcon={<KeyboardArrowDownIcon />}
+            variant="outlined"
+            color="black"
+            size="small"
+            sx={{
+              p: "0.6rem",
+            }}
+          >
+            <i className="fa-solid fa-user"></i>
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={handleRegister}>REGISTER</MenuItem>
+            <MenuItem onClick={handleLogin}>LOGIN</MenuItem>
+          </Menu>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            mr: "1.6rem",
+            ml: "1.6rem",
+          }}
+        >
+          <TextField
+            id="filled-basic"
+            placeholder="Search"
+            size="small"
+            sx={{
+              width: "40rem",
+            }}
+          />
+
+          <Button
+            variant="contained"
+            size="Normal"
+            color="black"
+            sx={{
+              color: "white",
+              fontSize: "1.6rem",
+            }}
+          >
+            Search
+          </Button>
         </Box>
       </nav>
       <div>
@@ -85,3 +203,6 @@ function Nav() {
 }
 
 export default Nav;
+
+//  <Link to="/register">REGISTER</Link>
+//   <Link to="/login">LOGIN</Link>
