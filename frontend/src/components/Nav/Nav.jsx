e5ff2967836e5556ea956d86671869afa28e0898
Trairat Kaewsaad <121665180+eurekaMarch@users.noreply.github.com>
2023-10-11 12:46:15 +0700

    update

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -35,166 +35,202 @@ function Nav() {
     <div>
       <nav>
         <Header />
-        {/* <Box className="Nav_PC"
-          sx={{
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "center",
-            mt: "1rem",
-          }}
-        >
+        <div className="Nav__pc">
           <Box
             sx={{
               display: "flex",
+              justifyContent: "space-around",
               alignItems: "center",
-              position: "relative",
-              width: "21rem",
+              mt: "1rem",
             }}
           >
-            <Link to="/">
-              <img width={70} src={Logo} alt="logo" />
-              <Typography
-                className="Nav_text-logo"
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                position: "relative",
+                width: "21rem",
+              }}
+            >
+              <Link to="/">
+                <img width={70} src={Logo} alt="logo" />
+                <Typography
+                  className="Nav_text-logo"
+                  sx={{
+                    fontSize: "3rem",
+                    fontWeight: "bold",
+                    position: "absolute",
+                    left: "6rem",
+                    top: "1.5rem",
+                  }}
+                >
+                  Shopeefy
+                </Typography>
+              </Link>
+            </Box>
+
+            <div className="Nav__input-pc">
+              <TextField
+                id="filled-basic"
+                placeholder="Search"
+                size="normal"
                 sx={{
-                  fontSize: "3rem",
-                  fontWeight: "bold",
-                  position: "absolute",
-                  left: "6rem",
-                  top: "1.5rem",
+                  width: "40rem",
+                }}
+              />
+
+              <Button
+                variant="contained"
+                color="black"
+                sx={{
+                  color: "white",
+                  fontSize: "1.6rem",
+                  height: "5.5rem",
                 }}
               >
-                Shopeefy
-              </Typography>
-            </Link>
+                Search
+              </Button>
+            </div>
+
+            <div className="Nav__input-mobile">
+              <TextField
+                id="filled-basic"
+                placeholder="Search"
+                size="small"
+                sx={{
+                  width: "25rem",
+                  fontSize: "1.2rem",
+                }}
+              />
+
+              <Button
+                variant="contained"
+                color="black"
+                size="normal"
+                sx={{
+                  color: "white",
+                  fontSize: "1.2rem",
+                }}
+              >
+                Search
+              </Button>
+            </div>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "15rem",
+              }}
+            >
+              <Link to="/register" id="Nav__register">
+                REGISTER
+              </Link>
+              <Link to="/login" id="Nav__login">
+                LOGIN
+              </Link>
+            </Box>
+          </Box>
+        </div>
+
+        {/* mobile */}
+        <div className="Nav__mobile">
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              mt: "1rem",
+              mr: "1.6rem",
+              ml: "0.8rem",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                position: "relative",
+                width: "14rem",
+              }}
+            >
+              <Link to="/">
+                <img width={60} src={Logo} alt="logo" />
+                <Typography
+                  className="Nav_text-logo"
+                  sx={{
+                    fontSize: "1.6rem",
+                    fontWeight: "bold",
+                    position: "absolute",
+                    left: "5.5rem",
+                    top: "2rem",
+                  }}
+                >
+                  Shopeefy
+                </Typography>
+              </Link>
+            </Box>
+            <Button
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+              endIcon={<KeyboardArrowDownIcon />}
+              variant="outlined"
+              color="black"
+              size="small"
+              sx={{
+                p: "0.6rem",
+              }}
+            >
+              <i className="fa-solid fa-user"></i>
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+              sx={{
+                fontSize: "1.4rem",
+              }}
+            >
+              <MenuItem onClick={handleRegister}>REGISTER</MenuItem>
+              <MenuItem onClick={handleLogin}>LOGIN</MenuItem>
+            </Menu>
           </Box>
 
-          <div>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              mx: "3rem",
+            }}
+          >
             <TextField
               id="filled-basic"
               placeholder="Search"
               size="small"
               sx={{
                 width: "40rem",
+                fontSize: "1.4rem",
               }}
             />
 
             <Button
               variant="contained"
-              size="Normal"
+              size="normal"
               color="black"
               sx={{
                 color: "white",
-                fontSize: "1.6rem",
+                fontSize: "1.4rem",
               }}
             >
               Search
             </Button>
-          </div>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              width: "15rem",
-            }}
-          >
-            <Link to="/register">REGISTER</Link>
-            <Link to="/login">LOGIN</Link>
           </Box>
-        </Box> */}
-
-        {/* mobile */}
-        <Box
-          className="Nav_mobile"
-          sx={{
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "center",
-            mt: "1rem",
-          }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              position: "relative",
-              width: "21rem",
-            }}
-          >
-            <Link to="/">
-              <img width={60} src={Logo} alt="logo" />
-              <Typography
-                className="Nav_text-logo"
-                sx={{
-                  fontSize: "1.6rem",
-                  fontWeight: "bold",
-                  position: "absolute",
-                  left: "5.5rem",
-                  top: "2rem",
-                }}
-              >
-                Shopeefy
-              </Typography>
-            </Link>
-          </Box>
-          <Button
-            id="basic-button"
-            aria-controls={open ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-            endIcon={<KeyboardArrowDownIcon />}
-            variant="outlined"
-            color="black"
-            size="small"
-            sx={{
-              p: "0.6rem",
-            }}
-          >
-            <i className="fa-solid fa-user"></i>
-          </Button>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-          >
-            <MenuItem onClick={handleRegister}>REGISTER</MenuItem>
-            <MenuItem onClick={handleLogin}>LOGIN</MenuItem>
-          </Menu>
-        </Box>
-
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            mr: "1.6rem",
-            ml: "1.6rem",
-          }}
-        >
-          <TextField
-            id="filled-basic"
-            placeholder="Search"
-            size="small"
-            sx={{
-              width: "40rem",
-            }}
-          />
-
-          <Button
-            variant="contained"
-            size="Normal"
-            color="black"
-            sx={{
-              color: "white",
-              fontSize: "1.6rem",
-            }}
-          >
-            Search
-          </Button>
-        </Box>
+        </div>
       </nav>
       <div>
         <Outlet />
@@ -204,6 +240,3 @@ function Nav() {
 }
 
 export default Nav;
-
-//  <Link to="/register">REGISTER</Link>
-//   <Link to="/login">LOGIN</Link>
